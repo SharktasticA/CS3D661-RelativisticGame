c4bb9a1a2864061f463414096c933f3aae5e5a42
SharktallicA <sharktallica@gmail.com>
2019-03-22 13:35:35 +0000

    Added quality levles and control
    Changelog:
     - Quality Settings: added low and medium settings as user options for quality
     - QualityControl: captures set quality level and applies additional quality-specific tasks

---
+++ Assets/Scripts/Misc/QualityControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+/// <summary>
+/// Enumarated possible quality levels the game uses.
+/// </summary>
+public enum QualityLevel { High = 0, Medium = 1, Low = 2 }
+
+/// <summary>
+/// Allows additional tasks to be performed at game
+/// start based on quality level.
+/// </summary>
+public class QualityControl : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    private QualityLevel quality;
+
+    private void Start()
+    {
+        quality = (QualityLevel)QualitySettings.GetQualityLevel();
+
+        // If quality is low, turn off PPS and lens flare
+        if (quality == QualityLevel.Low)
+        {
+            GetComponent<PostProcessLayer>().enabled = false;
+            GetComponent<FlareLayer>().enabled = false;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public QualityLevel GetQualityLevel() { return quality; }
+}
